84711cd89f1e7cd0467491d458a87897ced23b8b
keigo1401 <totutotu1015@gmail.com>
2021-08-14 13:57:53 +0000

    [Update]labelにfield_with_errorsのクラスが適用されない

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,13 +9,13 @@ class BooksController < ApplicationController
   end
 
   def create
-    @books = Book.all
-    @book = Book.new(book_params)
-    if @book.save
-     redirect_to book_path(book.id)
-    else
+   @book = Book.new(book_params)
+   if @book.save
+     redirect_to book_path(@book.id), notice: "Book was successfully created."
+   else
+     @books = Book.all
      render :index
-    end
+   end
   end
 
   def edit
@@ -23,18 +23,18 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
-     redirect_to book_path(book.id), notice: "Book was successfully updated."
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+     redirect_to book_path(@book.id), notice: "Book was successfully updated."
     else
-     render :show
+     render :edit
     end
   end
 
   def destroy
-    book = Book.find(params[:id])
-    book.destroy
-    redirect_to books_path
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path, notice: "Book was successfully destroyed."
   end
 
   private
